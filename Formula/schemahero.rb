c4c40d3709af0bcb629d78890cf71880dcffb6e2
marccampbell <173451+marccampbell@users.noreply.github.com>
2026-02-04 17:33:09 +0000

    Update formulas to v0.23.1

--- Formula/schemahero.rb
+++ Formula/schemahero.rb
@@ -4,28 +4,28 @@
 class Schemahero < Formula
   desc "Kubernetes-native database schema management"
   homepage "https://schemahero.io"
-  version "0.23.0"
+  version "0.23.1"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/schemahero_darwin_amd64.tar.gz"
-      sha256 "7d5b285f96f020e3ff9d7447240a7d90290eb3307de240940a123448588468c5"
+      sha256 "68191d86836af8186973f0efe5d8c11e774727f8ea0db9e57e7fe2262563c5ec"
     end
     on_arm do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/schemahero_darwin_arm64.tar.gz"
-      sha256 "9609d98d8b40cb8d4e84dfb176c2b9a24e8539db3d78f430885d91b92021b093"
+      sha256 "ea96744e263e06a5de04a2b84648c8c3eef0223d243c8bd6f0f8f8c2f7025615"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/schemahero_linux_amd64.tar.gz"
-      sha256 "84ed462ebb1d21baeee7f92d2ab08ef671a628fc22cfdd59a506bb5f04e5f947"
+      sha256 "e2ded2f5d4b692ef0b8047967d92430c78f7a34a9c08807c8718091e0a5880c3"
     end
     on_arm do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/schemahero_linux_arm64.tar.gz"
-      sha256 "cd28f2d4cd045532198327c36e3863c0af9534bda7e329bf988d427f4cacb433"
+      sha256 "4be456fa3ef5c6398475d21736df07a9fb0e73e73f87ab40654f89d5da0979d7"
     end
   end
 
