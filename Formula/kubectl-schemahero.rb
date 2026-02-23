a10a8ea68e646961ed9229930c309655ad5bffbf
marccampbell <173451+marccampbell@users.noreply.github.com>
2026-02-23 20:49:26 +0000

    Update formulas to v0.23.3

--- Formula/kubectl-schemahero.rb
+++ Formula/kubectl-schemahero.rb
@@ -4,28 +4,28 @@
 class KubectlSchemahero < Formula
   desc "Kubectl plugin for SchemaHero database schema management"
   homepage "https://schemahero.io"
-  version "0.23.2"
+  version "0.23.3"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/kubectl-schemahero_darwin_amd64.tar.gz"
-      sha256 "f6bb08c02a6bcc79f7888f903a6eab239f1c78a13b5018579acca48722aa715c"
+      sha256 "37379ab7b2f865cab9b93d8876806fb2293c64bb460337be345a3b8b50d9b309"
     end
     on_arm do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/kubectl-schemahero_darwin_arm64.tar.gz"
-      sha256 "b7547e83bc401d38fa2e6c8d07c995423eae81bad20e77a9c18d3aefc32af6b8"
+      sha256 "6a219cb404863348433120695049d971950e9ba8d5962842d303c69f462c3688"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/kubectl-schemahero_linux_amd64.tar.gz"
-      sha256 "b83a247d544711b81d3e5b1bc6c7b24d33b2c9b1fa381bb118bf853870beef2f"
+      sha256 "5a0c30241a19700ac6e3925c598f5b2655c955c553c122b304f9b44da9366ef5"
     end
     on_arm do
       url "https://github.com/schemahero/schemahero/releases/download/v#{version}/kubectl-schemahero_linux_arm64.tar.gz"
-      sha256 "c31acc75a4edbc334fdedb74acde387fc0ebd40d7ad59b91bef1b538eedc49a3"
+      sha256 "00efc53953c3dc9b526aa5b0093dd44722664481574b1940ca11b429be4a7f71"
     end
   end
 
